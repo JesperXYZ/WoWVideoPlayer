66d88bd26c4362ea352ef1f2d1c9e8d841b6c2b4
JesperXYZ <144018914+JesperXYZ@users.noreply.github.com>
2025-01-17 23:55:45 +0100

    only 1 test video

--- videos.lua
+++ videos.lua
@@ -2,37 +2,13 @@ local name, WoWVideoPlayer = ...
 
 WoWVideoPlayer.videos = {
     { 
-        name = "test_video_10fps_512x512", -- Name (it has to match the exact folder name of the video in the "stored_videos" folder)
+        name = "test_video_1", -- Name (it has to match the exact folder name of the video in the "stored_videos" folder)
         -- displayName = "Test Video",
         -- folderName = "test_video",
-        width = 960, -- Video resolution (this strecteches the video frames, the original resolutions MUST be 1024x1024)
+        width = 960, -- Video resolution (this strecteches the video frames, the original resolutions MUST be 1024x1024 or 512x512)
         height = 720,
         fps = 10,-- Images per second (if this is inaccurate, the video will be too fast or slow and audio will be desynced)
         totalFrameCount = 249, --
         autoPlayOnFlightPath = true, --
     },
-    {
-        name = "test_video_10fps_1024x1024",
-        width = 960,
-        height = 720,
-        fps = 10,
-        totalFrameCount = 249,
-        autoPlayOnFlightPath = true,
-    },
-    {
-        name = "test_video_20fps_512x512",
-        width = 960,
-        height = 720,
-        fps = 20,
-        totalFrameCount = 598,
-        autoPlayOnFlightPath = true,
-    },
-    {
-        name = "test_video_20fps_1024x1024",
-        width = 960,
-        height = 720,
-        fps = 20,
-        totalFrameCount = 598,
-        autoPlayOnFlightPath = true,
-    }
 }

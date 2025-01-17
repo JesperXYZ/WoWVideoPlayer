727d9ca89a2bcaf2f6f4c29b0a13561cb7e29531
JesperXYZ <144018914+JesperXYZ@users.noreply.github.com>
2025-01-17 20:34:34 +0100

    Update videos.lua

--- videos.lua
+++ videos.lua
@@ -4,6 +4,8 @@ WoWVideoPlayer.videos = {
     { 
         -- Name (it has to match the exact folder name of the video in the "stored_videos" folder)
         name = "test_video",
+        -- displayName = "Test Video",
+        -- folderName = "test_video",
 
         -- Video resolution (this strecteches the video frames, the original resolutions MUST be 1024x1024)
         width = 1920,
@@ -11,5 +13,12 @@ WoWVideoPlayer.videos = {
 
         -- Images per second (if this is inaccurate, the video will be too fast or slow and audio will be desynced)
         fps = 2,
+
+        -- 
+        autoPlayOnFlightPath = true
+
+        -- Makes it impossible to close the window while the video is playing,
+        -- esc key doesn't work either (/reload does work tho)
+        disableCloseWindowButton = true
     },
 }

f8792bf698a1087a122304b192a58d3a947b749e
JesperXYZ <144018914+JesperXYZ@users.noreply.github.com>
2025-01-18 06:58:28 +0100

    Update videos.lua

--- videos.lua
+++ videos.lua
@@ -1,15 +1,22 @@
 local name, WoWVideoPlayer = ...
 
 WoWVideoPlayer.videos = {
-    { 
-        name = "test_video_1", -- Name (it has to match the exact folder name of the video in the "stored_videos" folder)
-        -- displayName = "Test Video",
-        -- folderName = "test_video",
-        width = 960, -- Video resolution (this strecteches the video frames, the original resolutions MUST be 1024x1024 or 512x512)
+    {
+        -- Name (it has to match the exact folder name of the video in the "stored_videos" folder)
+        name = "test_video_1",
+
+        -- Video resolution (this strecteches the video frames, the original resolutions MUST be 1024x1024 or 512x512)
+        width = 960,
         height = 720,
-        fps = 10,-- Images per second (if this is inaccurate, the video will be too fast or slow and audio will be desynced)
-        totalFrameCount = 249, --
-        autoPlayOnFlightPath = true, --
+
+        -- Images per second (if this is inaccurate, the video will be too fast or slow and audio will be desynced)
+        fps = 10,
+
+        -- Total number of frames in the video (if this is inaccurate, the video will end too early or too late)
+        totalFrameCount = 249,
+        
+        -- Determine if the video automatically plays when the player is on a flight path
+        autoPlayOnFlightPath = true,
     },
     {
         name = "test_video_2",
